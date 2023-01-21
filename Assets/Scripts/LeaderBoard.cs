e572d8f159061933c9d1b07a7da89a04d72c4a82
Adam Hincu <adam@mediatonik.com>
2023-01-21 15:24:10 +0200

    LeadBoard Box.

--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 
-public class PlayfabManager : MonoBehaviour
+public class LeaderBoard : MonoBehaviour
 {
+	public GameObject RowPrefab;
+	public Transform rowsParent;
+
 	private void Start()
 	{
 		Login();
+
+		GetLeaderBoard();
 	}
 
 	void Login()
@@ -21,37 +27,15 @@ public class PlayfabManager : MonoBehaviour
 		PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
 	}
 
+
 	void OnSuccess(LoginResult result)
 	{
 		Debug.Log("Successful login/account create!");
 	}
-
 	void OnError(PlayFabError error)
 	{
 		Debug.Log("Error while logging in/creating account!");
 		Debug.Log(error.GenerateErrorReport());
-	}
-
-	public void SendLeadBoard(int score)
-	{
-		var request = new UpdatePlayerStatisticsRequest
-		{
-			Statistics = new List<StatisticUpdate>
-			{
-				new StatisticUpdate
-				{
-					StatisticName = "CPS",
-
-					Value = score
-				}
-			}
-		};
-		PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderBoardUpdate, OnError);
-	}
-
-	void OnLeaderBoardUpdate(UpdatePlayerStatisticsResult result)
-	{
-		Debug.Log("Successfull leaderboard sent");
 	}
 
 	public void GetLeaderBoard()
@@ -67,8 +51,21 @@ public class PlayfabManager : MonoBehaviour
 
 	void OnLeaderboardGet(GetLeaderboardResult result)
 	{
+		foreach (Transform item in rowsParent)
+		{
+			Destroy(item.gameObject);
+		}
+
 		foreach (var item in result.Leaderboard)
 		{
+			GameObject newGo = Instantiate(RowPrefab, rowsParent);
+
+			TMP_Text[] texts = newGo.GetComponentsInChildren<TMP_Text>();
+
+			texts[0].text = (item.Position + 1).ToString();
+			texts[1].text = item.PlayFabId;
+			texts[2].text = item.StatValue.ToString();
+
 			Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
 		}
 	}
